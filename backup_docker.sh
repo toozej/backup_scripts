4e30208b3bdf6e97e93622a9582d475f88980ed9
James Tooze <james@toozej.com>
2018-06-17 07:12:53 -0700

    use full path to docker-compose

--- backup_docker.sh
+++ backup_docker.sh
@@ -20,7 +20,7 @@ for DIR in `find /home/<username>/docker/ -mindepth 1 -maxdepth 1 -type d`; do
 	TAR_FILE_NAME="$(basename ${DIR})_${_NOW}.tgz"
 
 	# shutdown running docker container
-	docker-compose -f ${DIR}/docker-compose.yml down
+	/usr/local/bin/docker-compose -f ${DIR}/docker-compose.yml down
 	if [ $? -ne 0 ]; then
 		FAILED="${FAILED} shutting down ${DIR} docker container failed on ${_NOW}\n"
 		# skip to next ${DIR}
@@ -34,7 +34,7 @@ for DIR in `find /home/<username>/docker/ -mindepth 1 -maxdepth 1 -type d`; do
 	fi
 
 	# start docker container
-	docker-compose -f ${DIR}/docker-compose.yml up -d
+	/usr/local/bin/docker-compose -f ${DIR}/docker-compose.yml up -d
 	if [ $? -ne 0 ]; then
 		FAILED="${FAILED} starting ${DIR} docker container failed on ${_NOW}\n"
 	fi
